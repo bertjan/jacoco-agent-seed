4b82b7e297f41745fbc23add3b2d0b3f76c356b2
Bert Jan Schrijver <bertjan@revolution.nl>
2015-11-26 15:03:49 +0100

    Add installation script.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/bertjan/jacoco-agent-seed/master/coverage-report.sh
+wget https://raw.githubusercontent.com/bertjan/jacoco-agent-seed/master/coverage-setup.sh
+wget https://raw.githubusercontent.com/bertjan/jacoco-agent-seed/master/coverage.properties
+chmod +x coverage-report.sh
+chmod +x coverage-setup.sh
